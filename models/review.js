4ac0d4672a206d71adde965d4eb56c95f2ad96a3
abdullah <abdullamsuleman@gmail.com>
2026-07-21 14:56:36 +0300

    review model is added

--- models/review.js
+++ models/review.js
@@ -1,7 +1,27 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
- 
+ title: {
+  type: String,
+  required: true
+},
+author: {
+  type: String,
+  required: true
+},
+description: {
+  type: String,
+  required: true
+},
+coverImage: {
+  type: String,
+  required: true
+},
+publisher: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'User',
+  required: true
+}
 
 })
 
