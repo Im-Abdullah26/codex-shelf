a0b835dc1b958d0d81c3c6ac2f5b2757402e217d
abdullah <abdullamsuleman@gmail.com>
2026-07-21 15:36:31 +0300

    review model is updated

--- controllers/books.js
+++ controllers/books.js
@@ -1,4 +1,5 @@
 const Book = require('../models/book')
+const 
 
 const showNewForm = (req, res) => {
   res.render('books/new.ejs');
--- models/review.js
+++ models/review.js
@@ -1,23 +1,16 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
- title: {
+ text: {
   type: String,
   required: true
 },
-author: {
-  type: String,
+reviewer: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'User',
   required: true
 },
-description: {
-  type: String,
-  required: true
-},
-coverImage: {
-  type: String,
-  required: true
-},
-publisher: {
+book: {
   type: mongoose.Schema.Types.ObjectId,
   ref: 'User',
   required: true
